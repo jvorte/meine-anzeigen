675c7c043181d38be2bfe2db5ba1b4294f8c345a
Dimitris <jvortelinas@gmail.com>
2025-07-29 14:25:21 +0200

    γυρναω εδω αμα γινει μαλακκια με αλαγη σε car

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             CategoryTableSeeder::class,
              ElectronicBrandSeeder::class,
              MotorcycleBrandSeeder::class, 
+              CarBrandsAndModelsSeeder::class, 
         ]);
   
         
